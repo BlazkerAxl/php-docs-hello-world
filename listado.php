5ca1e22186c6ff0226df39ac89c9daee266239b0
BlazkerAxl <100676226+BlazkerAxl@users.noreply.github.com>
2022-10-05 13:04:41 -0500

    listado query

---
+++ listado.php
@@ -0,0 +1,17 @@
+<?php
+// PHP Data Objects(PDO) Connection:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:wqmsiasa.database.windows.net,1433; Database = wqms-iasadb", "wqmsiasa", "CalidadAgua2022");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);    
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+$sql = "SELECT TOP (15) * FROM dbo.iotwqmstable ORDER BY received_at DESC"; //Seleccion ultimo valor
+//$sql = "SELECT Temperatura, Turbidez, device_id, received_at FROM dbo.iotwqmstable ORDER BY received_at"; // Seleccion todos los valores
+foreach ($conn->query($sql) as $row) {
+    echo "\n Temperatura: " . $row["Temperatura"]. " - Turbidez: " . $row["Turbidez"]. " - Dispositivo: " . $row["device_id"]. "- Fecha: ".$row["received_at"]. "<br>";
+}
+?>
